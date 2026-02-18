7ea8f70d27b9e468b55f185e9d336dcb04afac19
Ch.Vasundhara <vasundhara_chemudupati@srmap.edu.in>
2026-02-18 08:16:21 +0530

    Fix root route

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 
 let rooms = {};
