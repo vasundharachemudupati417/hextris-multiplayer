797d6a4653908ac824d89cb26be2f35f856df050
Ch.Vasundhara <vasundhara_chemudupati@srmap.edu.in>
2026-02-18 08:35:38 +0530

    Fix binding

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => {
+
+server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
